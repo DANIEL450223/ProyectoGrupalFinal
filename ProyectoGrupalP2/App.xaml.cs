e2a165dccde119ebe10f9a4f21aa2c7bb4cb6da4
QuispeRonny <ronny.quispe@udla.edu.ec>
2025-07-13 14:57:32 -0500

    mportamos librerias,  definimos la clase principal  y conectamos con base de datos que s egardara en "parqueadero.db3"

--- ProyectoGrupalP2/App.xaml.cs
+++ ProyectoGrupalP2/App.xaml.cs
@@ -1,10 +1,19 @@
-﻿namespace ProyectoGrupalP2
+﻿using ProyectoGrupalP2.Services;
+using System.IO;
+namespace ProyectoGrupalP2
 {
     public partial class App : Application
     {
+        public static VehiculoRepository VehiculoRepo { get; private set; }
         public App()
         {
             InitializeComponent();
+            // ruta donde guardaremso la base de datos sqlite
+            string dbPath = Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+                "parqueadero.db3");
+
+            VehiculoRepo = new VehiculoRepository(dbPath);
 
             MainPage = new AppShell();
         }
---
+++ ProyectoGrupalP2/Services/VehiculoRepository.cs
@@ -0,0 +1,36 @@
+﻿using SQLite;
+using ProyectoGrupalP2.Models;
+
+namespace ProyectoGrupalP2.Services
+{
+    public class VehiculoRepository
+    {
+        string _dbPath;
+        private SQLiteConnection conn;
+
+        public VehiculoRepository(string dbPath)
+        {
+            _dbPath = dbPath;
+            Init();
+        }
+
+        private void Init()
+        {
+            if (conn != null)
+                return;
+
+            conn = new SQLiteConnection(_dbPath);
+            conn.CreateTable<Estacionamiento>();
+        }
+
+        public void AddVehiculo(Estacionamiento est)
+        {
+            conn.Insert(est);
+        }
+
+        public List<Estacionamiento> GetVehiculos()
+        {
+            return conn.Table<Estacionamiento>().ToList();
+        }
+    }
+}
